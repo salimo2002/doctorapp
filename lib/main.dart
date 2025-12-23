a5604213f308eb1c911adb784e0d37badd4a1ca4
salimo2002 <salimosalim31@gmail.com>
2025-12-22 16:08:03 -0800

    Create Nav Bar & Init Views

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,120 @@
+import 'package:doctorapp/view/home_view.dart';
+import 'package:doctorapp/widgets/custom_nav_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main(List<String> args) {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.black,
+      systemNavigationBarIconBrightness: Brightness.light,
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+    ),
+  );
   runApp(const DoctorApp());
 }
 
-class DoctorApp extends StatelessWidget {
+class DoctorApp extends StatefulWidget {
   const DoctorApp({super.key});
 
+  @override
+  State<DoctorApp> createState() => _DoctorAppState();
+}
+
+class _DoctorAppState extends State<DoctorApp> {
+  List<Widget> pages = [
+    HomeView(),
+    ChatView(),
+    HospitalsView(),
+    FavoriteView(),
+    ProfileView(),
+  ];
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: const Color.fromARGB(255, 99, 21, 82),
-          title: const Text('المستشار الطبي'),
-          titleTextStyle: TextStyle(
-            color: const Color.fromARGB(223, 240, 236, 236),
-            fontWeight: FontWeight.bold,
+        bottomNavigationBar: CustomNavBar(
+          currentIndex: currentIndex,
+          onTap: (index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+        ),
+        body: IndexedStack(index: currentIndex, children: pages),
+      ),
+    );
+  }
+}
+
+class HospitalsView extends StatelessWidget {
+  const HospitalsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Colors.blue.shade100, Colors.blue.shade50, Colors.white],
+        ),
+      ),
+    );
+  }
+}
+
+class FavoriteView extends StatelessWidget {
+  const FavoriteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Colors.blue.shade100, Colors.blue.shade50, Colors.white],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileView extends StatelessWidget {
+  const ProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blue.shade100, Colors.blue.shade50, Colors.white],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class ChatView extends StatelessWidget {
+  const ChatView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Colors.blue.shade100, Colors.blue.shade50, Colors.white],
         ),
       ),
     );
---
+++ lib/widgets/custom_nav_bar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class CustomNavBar extends StatelessWidget {
+  const CustomNavBar({
+    super.key,
+    required this.onTap,
+    required this.currentIndex,
+  });
+  final Function(int) onTap;
+  final int currentIndex;
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+      unselectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+      showUnselectedLabels: true,
+      elevation: 0,
+      iconSize: 28,
+      type: BottomNavigationBarType.fixed,
+      unselectedItemColor: Colors.grey,
+      backgroundColor: Colors.white,
+      selectedItemColor: Colors.blue,
+      currentIndex: currentIndex,
+      onTap: onTap,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          label: 'الرئيسية',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.chat_bubble_outline),
+          label: 'محادثة',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.local_hospital_outlined),
+          label: 'المشافي',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite_border),
+          label: 'المفضلة',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_2_outlined),
+          label: 'حسابي',
+        ),
+      ],
+    );
+  }
+}
